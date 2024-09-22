7ad4c5b8ba106c2b245d5aba1a3dac1caa94ade3
ayoub-benhammou-vinci <ayoub.benhammou@student.vinci.be>
2024-09-22 21:02:07 +0200

    Final configuration

--- tutorials/ts-node-start/back/api/basic/app.ts
+++ tutorials/ts-node-start/back/api/basic/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import { ErrorRequestHandler } from "express";
 
 import usersRouter from "./routes/users";
 import pizzaRouter from "./routes/pizzas";
+import drinkRouter from "./routes/drinks";
 
 const app = express();
 
@@ -10,5 +12,13 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/users", usersRouter);
 app.use("/pizzas", pizzaRouter);
+
+const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+    console.error(err.stack);
+    return res.status(500).send("Something broke!");
+  };
+  
+  app.use(errorHandler);
+  app.use("/drinks", drinkRouter);
 
 export default app;
