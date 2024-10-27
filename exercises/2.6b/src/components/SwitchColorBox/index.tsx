7db4a91c23de443371a5e7c65f830daed4e89547
ayoub-benhammou-vinci <ayoub.benhammou@student.vinci.be>
2024-10-27 14:14:46 +0100

    new:ex2.6b

---
+++ exercises/2.6b/src/components/SwitchColorBox/index.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import "./SwitchBox.css";
+
+const SwitchColorBox = () => {
+  //         Optimized Version in English
+  // setSwitchIndex = Change la valeur de SwitchIndex
+  // Si pas de setSwitchIndex : Variable local
+
+  const colorTab = ["red", "green", "blue", "yellow", "purple"];
+  const [switchIndex, setSwitchIndex] = useState(0);
+
+  return (
+    <div>
+        <p className={colorTab[switchIndex]} style={{color : colorTab[switchIndex]}}>
+            Actual color : {colorTab[switchIndex]}
+        </p>
+
+        <button onClick={() => setSwitchIndex((switchIndex+1)%colorTab.length)} className="boxSwitch">
+            <p>
+                The next color is {colorTab[(switchIndex+1)%colorTab.length]}
+            </p>
+        </button>
+
+    </div>
+  )
+
+  /*         My version if I write it in French 
+
+    const colorTab = ["rouge","vert","bleu", "jaune", "violet"];
+
+    // eslint-disable-next-line prefer-const
+    let [colorNumber, setColor] = useState(0);
+
+    const handleColor = () => {
+        colorNumber++;
+        colorNumber = colorNumber % colorTab.length;
+        setColor(colorNumber);
+    }
+
+    return (
+        <div>
+            <button onClick={handleColor} className="boxSwitch">
+                <p className={colorTab[colorNumber]}>Switch Box : Click on the box to change text-color</p>
+            </button>
+            <p>The next color is : {colorTab[(colorNumber + 1)%colorTab.length]}</p>
+        </div>
+    )
+    */
+};
+
+export default SwitchColorBox;
---
+++ exercises/2.6b/src/components/index.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import SwitchColorBox from './SwitchColorBox'
+
+function App() {
+
+  return (
+    <>
+    <h2>Change your color</h2>
+      <SwitchColorBox/>
+      <SwitchColorBox/>
+      <SwitchColorBox/>
+    </>
+  )
+}
+
+export default App
